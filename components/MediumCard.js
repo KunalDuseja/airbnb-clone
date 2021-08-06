b410f2c93ca3a37675d6b652e305fcbebf3944f2
Kunal Duseja <kunaliit123@gmail.com>
2021-08-07 00:46:56 +0530

    make it better

---
+++ components/MediumCard.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+function MediumCard({ item }) {
+  return (
+    <div className="cursor-pointer hover:scale-105 transform transition duration-300 ease-out">
+      <div className="relative h-80 w-80  ">
+        <Image src={item.img} layout="fill" className="rounded-xl" />
+      </div>
+
+      <h1 className="text-2xl mt-3" >{item.title}</h1>
+    </div>
+  );
+}
+
+export default MediumCard;
